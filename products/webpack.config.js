c254952c41a0a52c4f2c1ab5424c1cbdc85c57f8
kusham <kusham1998@gmail.com>
2023-02-26 02:00:03 +0530

    add shared option

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
       exposes: {
         "./ProductIndex": "./src/index",
       },
+      shared: ["faker"],
     }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",

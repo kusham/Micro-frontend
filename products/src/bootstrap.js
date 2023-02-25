ee9ee91d4dfb79333e189898ee1c65003c41ba52
kusham <kusham1998@gmail.com>
2023-02-26 02:26:59 +0530

    implement mount

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -13,4 +13,25 @@ const mount = (el) => {
 };
 
 export { mount };
+
 // context/situation #1
+// we are running this file in development in isolation
+// we are using out local index.html file
+// which DEFINITELY has an element with an id of 'dev-product'
+// we want to immediately render our app into that element
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("dev-products-dev-only");
+  // assuming our container does not have an element
+  // with id 'dev-products-dev-only'
+  
+  if (el) {
+      // we are probably running in isolation
+      mount(el);
+  }
+}
+
+// context/situation #2
+// we are running this file in development or production
+// through the container app
+// no guarantee that an element with an id of 'dev-product'
+// we do not want to immediately render our app into that element

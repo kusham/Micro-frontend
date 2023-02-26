232350f1c0c82d58601113bdf8460f2d158ae5f6
kusham <kusham1998@gmail.com>
2023-02-26 14:03:30 +0530

    change index to bootstrap

--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
       name: "cart",
       filename: "remoteEntry.js",
       exposes: {
-        "./CartIndex": "./src/index",
+        "./CartIndex": "./src/bootstrap",
       },
       shared: ["faker"],
     }),

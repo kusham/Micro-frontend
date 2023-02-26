ad84642d924a6bbcdb208e8850f4c5ce27f353df
kusham <kusham1998@gmail.com>
2023-02-26 14:04:22 +0530

    import mount function

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,11 @@
-import 'products/ProductIndex'
-import 'cart/CartIndex'
+import "products/ProductIndex";
+import "cart/CartIndex";
+import { mount as productMount } from "../../products/src/bootstrap";
+import { mount as cartMount } from "../../cart/src/bootstrap";
 
-document.querySelector('#dev-container').innerHTML = `<div>Hello world</div><br></br>`
+productMount(document.querySelector("#my-products"));
+cartMount(document.querySelector("#my-cart"));
+
+document.querySelector(
+  "#dev-container"
+).innerHTML = `<div>Hello world</div><br></br>`;

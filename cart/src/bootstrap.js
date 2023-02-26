0ed43f664fb4ff19f34ca6da7b3172c449b202b5
kusham <kusham1998@gmail.com>
2023-02-26 14:04:39 +0530

    create mount

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,12 +1,25 @@
-import faker from 'faker'
+import faker from "faker";
 
-let products = ''
+const mount = (el) => {
+  let products = "";
 
-for (let index = 0; index < 30; index++) {
-   
+  for (let index = 0; index < 30; index++) {
     const name = faker.commerce.productName();
-    products += `<div>${name}</div>`
-}
-console.log("object");
+    products += `<div>${name}</div>`;
+  }
 
-document.querySelector('#dev-cart').innerHTML = '<br></br><div>This is Your cart</div>';
+  el.innerHTML = "<br></br><div>This is Your cart</div>";
+};
+
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#dev-cart-dev-only");
+  // assuming our container does not have an element
+  // with id 'dev-products-dev-only'
+
+  if (el) {
+    // we are probably running in isolation
+    mount(el);
+  }
+}
+
+export { mount };
